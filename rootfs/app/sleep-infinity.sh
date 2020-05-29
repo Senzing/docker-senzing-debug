d432955ef9134862c5a241f149aae9e397e2022f
Michael Dockter <michael@dockter.com>
2020-05-29 17:21:33 -0400

    #32 Optional mysql client at runtime

--- rootfs/app/sleep-infinity.sh
+++ rootfs/app/sleep-infinity.sh
@@ -14,6 +14,14 @@ trap 'handle_term' TERM
 # Main
 # =============================================================================
 
+if [[ ( ! -z ${INSTALL_MYSQL_CLIENT} ) ]]; then
+    echo "Installing mysql-client."
+
+    apt-get update
+    wget https://dev.mysql.com/get/Downloads/Connector-ODBC/8.0/mysql-connector-odbc_8.0.20-1debian10_amd64.deb
+    apt install ./mysql-connector-odbc_8.0.20-1debian10_amd64.deb
+fi
+
 echo "/app/sleep-infinitely.sh is sleeping infinitely."
 
 # Sleep in a manner that allows "docker stop ..." to shutdown gracefully.
